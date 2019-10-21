556e0ce9a61add7a894101278a6a206c05f6ae9a
Mark Alston <malston@pivotal.io>
2019-10-21 09:37:41 -0600

    Initial commit

---
+++ install-chart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+namespace="${1:?"First argument must be a namespace"}"
+
+kubectl create namespace "${namespace}"
+
+kubectl config set-context --current --namespace="${namespace}"
+
+# Create CRDs
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/podmonitor.crd.yaml
+
+# Install operator
+helm install prometheus stable/prometheus-operator \
+    --set prometheusOperator.createCustomResource=false
+
+# Port forward services
+kubectl --namespace "${namespace}" port-forward svc/prometheus-grafana 3000:80
+kubectl --namespace "${namespace}" port-forward svc/alertmanager-operated 9093
+kubectl --namespace "${namespace}" port-forward svc/prometheus-prometheus-oper-prometheus 9090
+
---
+++ install-helm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eou pipefail
+
+function install_helm_cli {
+  version="${HELM_CLI_VERSION:-v3.0.0-beta.4}"
+  os="${OS:-darwin}"
+  arch="${ARCH:-${arch}}"
+  file="helm.tar.gz"
+  trap "{ rm -f $file ; exit 255; }" EXIT
+  wget -O $file https://get.helm.sh/helm-${version}-${os}-${arch}.tar.gz
+  tar -zxvf $file --strip=1 -C /tmp
+  chmod +x /tmp/helm
+  sudo mv /tmp/helm /usr/local/bin/helm
+  rm $file
+  type helm
+}
+
+echo "Enter your Operating System (linux, darwin, windows): "
+read -r OS
+
+echo "Enter your OS Architecture (amd64, 386): "
+read -r ARCH
+
+install_helm_cli
