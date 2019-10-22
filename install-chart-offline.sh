2dfd93b8b8e103a09cd7fc60d3be4aea11e93a41
Mark Alston <malston@pivotal.io>
2019-10-22 08:07:03 -0600

    Add crds for offline install

--- install-chart-offline.sh
+++ install-chart-offline.sh
@@ -7,16 +7,13 @@ kubectl create namespace "${namespace}"
 kubectl config set-context --current --namespace="${namespace}"
 
 # Create CRDs
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/podmonitor.crd.yaml
+kubectl apply -f helm/prometheus-operator/crds/
 
 # Install operator
 helm template helm/prometheus-operator \
     --namespace "${namespace}" \
-    --set prometheusOperator.createCustomResource=false | kubectl apply -f -
+    --set prometheusOperator.createCustomResource=false \
+    | kubectl apply -f -
 
 # Port forward services
 # kubectl --namespace "${namespace}" port-forward svc/prometheus-grafana 3000:80
