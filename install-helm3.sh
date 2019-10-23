7cf8c00dda62efe35ffdd1ce4032e0ce2f7568eb
Mark Alston <malston@pivotal.io>
2019-10-23 09:09:46 -0600

    Create separate script to install helm3

--- install-helm3.sh
+++ install-helm3.sh
@@ -3,20 +3,17 @@
 set -eou pipefail
 
 function install_helm_cli {
-  version="${HELM_CLI_VERSION:-v2.14.1}"
+  version="${HELM_CLI_VERSION:-v3.0.0-beta.4}"
   os="${OS:-darwin}"
   arch="${ARCH:-amd64}"
   file="helm.tar.gz"
   trap "{ rm -f "$file" ; exit 255; }" EXIT
-  wget -O $file https://storage.googleapis.com/kubernetes-helm/helm-${version}-${os}-${arch}.tar.gz
+  wget -O $file https://get.helm.sh/helm-${version}-${os}-${arch}.tar.gz
   tar -zxvf $file --strip=1 -C /tmp
   chmod +x /tmp/helm
-  chmod +x /tmp/tiller
   sudo mv /tmp/helm /usr/local/bin/helm
-  sudo mv /tmp/tiller /usr/local/bin/tiller
   rm $file
   type helm
-  type tiller
 }
 
 install_helm_cli
