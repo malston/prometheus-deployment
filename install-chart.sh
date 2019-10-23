16d65cd7b0c8821b42a51d5233f9000c5e923b99
Mark Alston <malston@pivotal.io>
2019-10-23 15:32:37 -0600

    Update helm3

--- install-chart.sh
+++ install-chart.sh
@@ -14,8 +14,17 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/ma
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/podmonitor.crd.yaml
 
 # Install operator
-helm install prometheus stable/prometheus-operator \
+helm3 install prometheus stable/prometheus-operator \
+    --set global.rbac.pspEnabled=false \
+    --set grafana.adminPassword=admin \
     --set prometheusOperator.createCustomResource=false
+
+kubectl expose deployment "$(kubectl get deployments -o jsonpath="{.items[0].metadata.name}")" \
+    --name=prometheus-grafana-lb \
+    --port=80 \
+    --target-port=3000 \
+    --type=LoadBalancer \
+    --namespace="${namespace}"
 
 # Port forward services
 # kubectl --namespace "${namespace}" port-forward svc/prometheus-grafana 3000:80
