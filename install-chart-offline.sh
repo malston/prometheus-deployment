97a58c35f7561e66e9058b2f910f0107118bdb86
Mark Alston <malston@pivotal.io>
2019-10-23 14:05:02 -0600

    Add configmaps for all the dashboards for the sidecar to pick up

--- install-chart-offline.sh
+++ install-chart-offline.sh
@@ -9,6 +9,9 @@ kubectl config set-context --current --namespace="${namespace}"
 # Create CRDs
 kubectl apply -f helm/prometheus-operator/crds/
 
+# Create Dashboard ConfigMaps
+kubectl apply -f helm/prometheus-operator/configmaps/
+
 # Install operator
 helm template helm/prometheus-operator \
     -f helm/prometheus-operator/offline-values.yaml \
@@ -22,10 +25,10 @@ helm template helm/prometheus-operator \
     --set grafana.adminPassword=admin \
     --set grafana.testFramework.enabled=false \
     --set defaultDashboardsEnabled=false \
-    --set sidecar.dashboards.enabled=false \
+    --set sidecar.dashboards.enabled=true \
     | kubectl apply -f -
 
-kubectl expose deployment "$(kubectl get deployments -o jsonpath="{.items[0].metadata.name}")" --name=prometheus-grafana-lb --port=80 --target-port=3000 --type=LoadBalancer --namespace="${namespace}"
+# kubectl expose deployment "$(kubectl get deployments -o jsonpath="{.items[0].metadata.name}")" --name=prometheus-grafana-lb --port=80 --target-port=3000 --type=LoadBalancer --namespace="${namespace}"
 
 # Port forward services
 # kubectl --namespace "${namespace}" port-forward svc/prometheus-grafana 3000:80
