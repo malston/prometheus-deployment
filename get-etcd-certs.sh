f6198d34a91bc80bbdf6a09c987f950b544d6bb9
Mark Alston <malston@pivotal.io>
2019-10-30 10:51:29 -0600

    Rename etcd client ca file

--- get-etcd-certs.sh
+++ get-etcd-certs.sh
@@ -7,3 +7,5 @@ set -o pipefail
 deployment=${1:?"bosh deployment name for service instance of the cluster"}
 
 bosh -d "${deployment}" scp "master/0:/var/vcap/jobs/kube-apiserver/config/etc*" .
+
+mv etcd-ca.crt etcd-client-ca.crt
