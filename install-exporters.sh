ff1b795cd54e6c08f2c76e5ed35eeb537cb89b18
Mark Alston <malston@pivotal.io>
2019-10-30 17:38:04 -0600

    Change pod to deployment

--- install-exporters.sh
+++ install-exporters.sh
@@ -9,4 +9,8 @@ kubectl create secret -n "${namespace}" generic bosh-ca \
 
 kubectl create -f exporters/bosh-exporter.yaml
 
-kubectl exec -it $(kubectl get pod -l app=bosh-exporter -o jsonpath='{.items[0].metadata.name}') -c bosh-exporter -- wget bosh-exporter:9190/metrics -O metrics.txt
+sleep 4
+
+kubectl logs $(kubectl get pod -l app=bosh-exporter -o jsonpath='{.items[0].metadata.name}')
+
+# kubectl exec -it $(kubectl get pod -l app=bosh-exporter -o jsonpath='{.items[0].metadata.name}') -c bosh-exporter -- wget bosh-exporter:9190/metrics
