b6ce1c022335a77b5ae114bd6fc1c9f5ed7df831
Mark Alston <malston@pivotal.io>
2019-12-10 12:29:11 -0700

    Add send_alert.sh for testing alerts

---
+++ send_alert.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+alerts1='[
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sda1",
+       "instance": "example1"
+     },
+     "annotations": {
+        "info": "The disk sda1 is running full",
+        "summary": "please check the instance example1"
+      }
+  },
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sda2",
+       "instance": "example1"
+     },
+     "annotations": {
+        "info": "The disk sda2 is running full",
+        "summary": "please check the instance example1",
+        "runbook": "the following link http://test-url should be clickable"
+      }
+  },
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sda1",
+       "instance": "example2"
+     },
+     "annotations": {
+        "info": "The disk sda1 is running full",
+        "summary": "please check the instance example2"
+      }
+  },
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sdb2",
+       "instance": "example2"
+     },
+     "annotations": {
+        "info": "The disk sdb2 is running full",
+        "summary": "please check the instance example2"
+      }
+  },
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sda1",
+       "instance": "example3",
+       "severity": "critical"
+     }
+  },
+  {
+    "labels": {
+       "alertname": "CPUThrottlingHigh",
+       "dev": "sda1",
+       "instance": "example3",
+       "severity": "warning"
+     }
+  }
+]'
+curl -XPOST -d"$alerts1" http://alertmanager-03.haas-440.pez.pivotal.io/api/v1/alerts
