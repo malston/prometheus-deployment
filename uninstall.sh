0b873d88550290c78e81a4e6afb4e6fca2f56118
Mark Alston <malston@pivotal.io>
2020-01-07 13:25:11 -0700

    Remove legacy ref to manifests dir

--- uninstall.sh
+++ uninstall.sh
@@ -7,8 +7,6 @@ helm uninstall "${release}"
 
 kubectl delete secret -n "${namespace}" etcd-client --ignore-not-found
 
-kubectl delete --recursive --filename ./manifests/prometheus-operator --ignore-not-found
-
 kubectl delete --recursive --filename ./templates --ignore-not-found
 
 kubectl delete pvc prometheus-monitoring-prometheus-oper-prometheus-db-prometheus-monitoring-prometheus-oper-prometheus-0
