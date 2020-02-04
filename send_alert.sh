6814d1c74064aee0a89688ca592f19026b26d07d
Mark Alston <malston@pivotal.io>
2020-02-04 11:37:38 -0700

    Update test alert config and script

--- send_alert.sh
+++ send_alert.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-alerts1='[
+CLUSTER_NAME="$(kubectl config current-context)"
+CLUSTER_NUM="$(echo "${CLUSTER_NAME}" | cut -c8-9)"
+ALERTMANAGER_URL="https://alertmanager-${CLUSTER_NUM}.haas-440.pez.pivotal.io"
+
+firing_alerts='[
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sda1",
@@ -13,6 +18,7 @@ alerts1='[
       }
   },
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sda2",
@@ -25,6 +31,7 @@ alerts1='[
       }
   },
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sda1",
@@ -36,6 +43,7 @@ alerts1='[
       }
   },
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sdb2",
@@ -47,6 +55,7 @@ alerts1='[
       }
   },
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sda1",
@@ -55,6 +64,7 @@ alerts1='[
      }
   },
   {
+    "status": "firing",
     "labels": {
        "alertname": "TestAlert",
        "dev": "sda1",
@@ -63,4 +73,80 @@ alerts1='[
      }
   }
 ]'
-curl -XPOST -d"$alerts1" http://alertmanager-01.haas-440.pez.pivotal.io/api/v1/alerts
+echo ""
+curl -XPOST -d"$firing_alerts" -k "$ALERTMANAGER_URL/api/v1/alerts"
+echo ""
+
+resolved_alerts='[
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sda1",
+       "instance": "example1"
+     },
+     "annotations": {
+        "info": "The disk sda1 is running full",
+        "summary": "please check the instance example1"
+      }
+  },
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sda2",
+       "instance": "example1"
+     },
+     "annotations": {
+        "info": "The disk sda2 is running full",
+        "summary": "please check the instance example1",
+        "runbook": "the following link http://test-url should be clickable"
+      }
+  },
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sda1",
+       "instance": "example2"
+     },
+     "annotations": {
+        "info": "The disk sda1 is running full",
+        "summary": "please check the instance example2"
+      }
+  },
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sdb2",
+       "instance": "example2"
+     },
+     "annotations": {
+        "info": "The disk sdb2 is running full",
+        "summary": "please check the instance example2"
+      }
+  },
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sda1",
+       "instance": "example3",
+       "severity": "critical"
+     }
+  },
+  {
+    "status": "resolved",
+    "labels": {
+       "alertname": "TestAlert",
+       "dev": "sda1",
+       "instance": "example3",
+       "severity": "warning"
+     }
+  }
+]'
+echo -n "Press enter to resolve 'TestAlert'"
+read -r
+curl -XPOST -d"$resolved_alerts" -k "$ALERTMANAGER_URL/api/v1/alerts"
+echo ""
