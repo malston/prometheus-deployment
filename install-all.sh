29df1b8b4f4ea71ae33cf7673ae148cff046f715
Mark Alston <malston@pivotal.io>
2020-02-12 16:21:15 -0700

    Refactor install scripts

--- install-all.sh
+++ install-all.sh
@@ -34,39 +34,6 @@ source "${__DIR}/scripts/target-bosh.sh"
 
 clusters="$(pks clusters --json | jq 'sort_by(.name)' | jq -r .[].name)"
 for cluster in ${clusters}; do
-  kubectl config set-context "${cluster}"
-  
-  if [[ ! $(kubectl get namespace "${namespace}") ]]; then
-    kubectl create namespace "${namespace}"
-  fi
-  kubectl config set-context "${cluster}" --namespace="${namespace}"
-
-  # Create storage class
-  kubectl delete storageclass thin-disk --ignore-not-found
-  kubectl create -f storage/storage-class.yaml
-
-  ./create-secrets.sh "${foundation}" "${cluster}" "${namespace}"
-
-  ./interpolate.sh "${foundation}" "${cluster}"
-
-  # Copy dashboards to grafana chart location
-  cp dashboards/*.json charts/prometheus-operator/charts/grafana/dashboards/
-
-  bosh_exporter_enabled="$(om interpolate -s --config "environments/${foundation}/config/config.yml" --vars-file "environments/${foundation}/vars/vars.yml" --vars-env VARS --path "/clusters/cluster_name=${cluster}/bosh_exporter_enabled")"
-  pks_monitor_enabled="$(om interpolate -s --config "environments/${foundation}/config/config.yml" --vars-file "environments/${foundation}/vars/vars.yml" --vars-env VARS --path "/clusters/cluster_name=${cluster}/pks_monitor_enabled")"
-  
-  # Install prometheus-operator
-  helm upgrade -i --version "${version}" "${release}" \
-      --namespace "${namespace}" \
-      --values /tmp/overrides.yaml \
-      --set bosh-exporter.boshExporter.enabled="${bosh_exporter_enabled}" \
-      --set pks-monitor.pksMonitor.enabled="${pks_monitor_enabled}" \
-      --set global.rbac.pspEnabled=false \
-      --set grafana.adminPassword=admin \
-      --set grafana.testFramework.enabled=false \
-      --set kubeTargetVersionOverride="$(kubectl version --short | grep -i server | awk '{print $3}' |  cut -c2-1000)" \
-      ./charts/prometheus-operator
-
-  # Remove copied dashboards
-  rm charts/prometheus-operator/charts/grafana/dashboards/*.json
+  # shellcheck disable=SC1091
+  source ./install_cluster.sh "${cluster}" "${foundation}" "${namespace}" "${release}" "${version}"
 done
---
+++ install_cluster.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+cluster="${1:?"Cluster name is required"}"
+foundation="${2:?"Foundation name is required"}"
+namespace="${3:?"Namespace is required"}"
+release="${4:?"Release is required"}"
+version="${5:?"Version is required"}"
+
+kubectl config set-context "${cluster}"
+
+if [[ ! $(kubectl get namespace "${namespace}") ]]; then
+kubectl create namespace "${namespace}"
+fi
+kubectl config set-context "${cluster}" --namespace="${namespace}"
+
+# Create storage class
+kubectl delete storageclass thin-disk --ignore-not-found
+kubectl create -f storage/storage-class.yaml
+
+./create-secrets.sh "${foundation}" "${cluster}" "${namespace}"
+
+./interpolate.sh "${foundation}" "${cluster}"
+
+# Copy dashboards to grafana chart location
+cp dashboards/*.json charts/prometheus-operator/charts/grafana/dashboards/
+
+bosh_exporter_enabled="$(om interpolate -s --config "environments/${foundation}/config/config.yml" --vars-file "environments/${foundation}/vars/vars.yml" --vars-env VARS --path "/clusters/cluster_name=${cluster}/bosh_exporter_enabled")"
+pks_monitor_enabled="$(om interpolate -s --config "environments/${foundation}/config/config.yml" --vars-file "environments/${foundation}/vars/vars.yml" --vars-env VARS --path "/clusters/cluster_name=${cluster}/pks_monitor_enabled")"
+
+# Install prometheus-operator
+helm upgrade -i --version "${version}" "${release}" \
+	--namespace "${namespace}" \
+	--values /tmp/overrides.yaml \
+	--set bosh-exporter.boshExporter.enabled="${bosh_exporter_enabled}" \
+	--set pks-monitor.pksMonitor.enabled="${pks_monitor_enabled}" \
+	--set global.rbac.pspEnabled=false \
+	--set grafana.adminPassword=admin \
+	--set grafana.testFramework.enabled=false \
+	--set kubeTargetVersionOverride="$(kubectl version --short | grep -i server | awk '{print $3}' |  cut -c2-1000)" \
+	./charts/prometheus-operator
+
+# Remove copied dashboards
+rm charts/prometheus-operator/charts/grafana/dashboards/*.json
