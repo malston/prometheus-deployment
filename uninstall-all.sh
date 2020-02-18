cb73d5b92b815db6a870d551cbf1d7e78b228153
Mark Alston <malston@pivotal.io>
2020-02-18 09:18:59 -0700

    Fix release param in uninstall-all.sh

--- uninstall-all.sh
+++ uninstall-all.sh
@@ -4,12 +4,12 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
-release="${3:-prometheus-operator}"
+release="${1:-prometheus-operator}"
 
 clusters="$(pks clusters --json | jq 'sort_by(.name)' | jq -r .[].name)"
 
 for cluster in ${clusters}; do
     kubectl config use-context "${cluster}"
-    printf "Unstalling Prometheus Operator from %s\n" "${cluster}"
+    printf "Unstalling %s from %s\n" "${release}" "${cluster}"
     helm uninstall "${release}"
 done
