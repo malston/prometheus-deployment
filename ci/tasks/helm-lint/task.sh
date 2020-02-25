17c435dbcb4051549dffb3b07aee26d332fbe72e
Mark Alston <malston@pivotal.io>
2020-02-25 10:04:38 -0700

    Switch into namespace before lint/test

--- ci/tasks/helm-lint/task.sh
+++ ci/tasks/helm-lint/task.sh
@@ -3,10 +3,16 @@
 function main() {
   local repo="${1}"
   local release_name="${2}"
+  local namespace="${2}"
+
   cd "${repo}/charts/${release_name}" || exit
+
   clusters="$(pks clusters --json | jq 'sort_by(.name)' | jq -r .[].name)"
+
   for cluster in ${clusters}; do
     pks get-credentials "${cluster}"
+
+    switch_namespace "${cluster}" "${namespace}"
 
     printf "Linting %s on %s\n" "${release_name}" "${cluster}"
     helm lint
@@ -21,10 +27,20 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+source "${__DIR}/../../../scripts/helpers.sh"
+
 release="${1:-$RELEASE}"
 
 if [[ -z "${release}" ]]; then
   echo "Release is required"
+  exit 1
+fi
+
+if [[ -z "${namespace}" ]]; then
+  echo "Namespace name is required"
   exit 1
 fi
 
@@ -34,4 +50,4 @@ cp pks-config/creds.yml ~/.pks/creds.yml
 mkdir -p ~/.kube
 cp kube-config/config ~/.kube/config
 
-main "repo" "${release}"
+main "repo" "${release}" "${namespace}"
--- ci/tasks/helm-test/task.sh
+++ ci/tasks/helm-test/task.sh
@@ -2,9 +2,14 @@
 
 function main() {
   local release_name="${1}"
+  local namespace="${2}"
+
   clusters="$(pks clusters --json | jq 'sort_by(.name)' | jq -r .[].name)"
+
   for cluster in ${clusters}; do
     pks get-credentials "${cluster}"
+
+    switch_namespace "${cluster}" "${namespace}"
 
     release="$(helm list -q -f "${release_name}")"
     if [[ -z "${release}" ]]; then
@@ -24,10 +29,20 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck disable=SC1090
+source "${__DIR}/../../../scripts/helpers.sh"
+
 release="${1:-$RELEASE}"
 
 if [[ -z "${release}" ]]; then
   echo "Release is required"
+  exit 1
+fi
+
+if [[ -z "${namespace}" ]]; then
+  echo "Namespace name is required"
   exit 1
 fi
 
@@ -37,4 +52,4 @@ cp pks-config/creds.yml ~/.pks/creds.yml
 mkdir -p ~/.kube
 cp kube-config/config ~/.kube/config
 
-main "${release}"
+main "${release}" "${namespace}"
