c40008a05e9a0f0c242072f4dc3a28645a50a874
Mark Alston <malston@pivotal.io>
2020-02-26 20:29:20 -0700

    Create a task for searching latest version

---
+++ ci/tasks/helm-search/task.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function main() {
+	printf "Checking for newest version of chart\n"
+	
+	helm search repo prometheus-operator -o json \
+		| jq -r '.[] | select(.name=="stable/prometheus-operator") | .version' \
+		> ../version/version
+}
+
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+main
