eb6e4b13bca2c5080192929031948dc90994af5e
Mark Alston <malston@pivotal.io>
2020-03-20 12:50:22 -0600

    Add code to use vendor dir

--- find-images.py
+++ find-images.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+
+sys.path.append(vendor_dir)
+
 import yaml
 
 def find(key, dictionary):
