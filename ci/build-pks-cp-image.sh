fccd3bc2aa890c8e4df392c8b52be33ae710bee8
Mark Alston <malston@pivotal.io>
2020-03-20 12:49:41 -0600

    Push image to docker instead of harbor for now

--- ci/build-pks-cp-image.sh
+++ ci/build-pks-cp-image.sh
@@ -14,9 +14,9 @@ docker_image_tag="0.0.1"
 
 docker build --build-arg pivnet_token="${pivnet_token}" --build-arg opsman_ssh_key="$(cat "${opsman_ssh_key}")" -f ./docker-files/Dockerfile.pks-control-plane -t "${docker_image_name}:${docker_image_tag}" .
 
-# docker login
-# docker tag "${docker_image_name}:${docker_image_tag}" "${docker_image_name}:${docker_image_tag}"
-# docker push "${docker_image_name}:${docker_image_tag}"
-docker login "https://${HARBOR_HOSTNAME}" --username admin --password "${HARBOR_ADMIN_PASSWORD}"
-docker tag "${docker_image_name}:${docker_image_tag}" "${HARBOR_HOSTNAME}/library/${docker_image_name}:${docker_image_tag}"
-docker push "${HARBOR_HOSTNAME}/library/${docker_image_name}:${docker_image_tag}"
+docker login
+docker tag "${docker_image_name}:${docker_image_tag}" "${docker_image_name}:${docker_image_tag}"
+docker push "${docker_image_name}:${docker_image_tag}"
+# docker login "https://${HARBOR_HOSTNAME}" --username admin --password "${HARBOR_ADMIN_PASSWORD}"
+# docker tag "${docker_image_name}:${docker_image_tag}" "${HARBOR_HOSTNAME}/library/${docker_image_name}:${docker_image_tag}"
+# docker push "${HARBOR_HOSTNAME}/library/${docker_image_name}:${docker_image_tag}"
