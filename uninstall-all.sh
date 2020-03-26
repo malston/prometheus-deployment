f6895f02b7e96f9a39572780f5d5416e89ec8969
Mark Alston <malston@pivotal.io>
2020-03-26 10:49:43 -0600

    Keep uninstalling if release not there

--- uninstall-all.sh
+++ uninstall-all.sh
@@ -11,5 +11,7 @@ clusters="$(pks clusters --json | jq -r 'sort_by(.name) | .[] | select(.last_act
 for cluster in ${clusters}; do
     kubectl config use-context "${cluster}"
     printf "Uninstalling %s from %s\n" "${release}" "${cluster}"
-    helm uninstall "${release}"
+    helm uninstall "${release}"  > /dev/null 2>&1 || continue
+    kubectl delete configmap bosh-target-groups
+    kubectl delete pvc prometheus-prometheus-operator-prometheus-db-prometheus-prometheus-operator-prometheus-0
 done
