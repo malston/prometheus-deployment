089c934da2371248b8831845288ae3443762072d
Mark Alston <malston@pivotal.io>
2020-03-30 08:53:02 -0600

    Remove namespace prefix

--- forward-services.sh
+++ forward-services.sh
@@ -3,6 +3,6 @@
 namespace="${1:-"monitoring"}"
 
 # Port forward services
-kubectl --namespace "${namespace}" port-forward svc/monitoring-grafana 3000:80 &
+kubectl --namespace "${namespace}" port-forward svc/grafana 3000:80 &
 kubectl --namespace "${namespace}" port-forward svc/alertmanager-operated 9093 &
-kubectl --namespace "${namespace}" port-forward svc/monitoring-prometheus-oper-prometheus 9090 &
+kubectl --namespace "${namespace}" port-forward svc/prometheus-oper-prometheus 9090 &
