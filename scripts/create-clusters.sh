4ca5bec362c0079bf923641feedb0e65d3122923
Mark Alston <malston@pivotal.io>
2020-04-06 08:47:15 -0600

    Create script for creating 3 small clusters

---
+++ scripts/create-clusters.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+external_hostname="${1:-${EXTERNAL_HOSTNAME}}"
+
+if [ -z "${external_hostname}" ]; then
+  echo "Enter cluster domain: (e.g., markalston.net)"
+  read -r external_hostname
+fi
+
+for i in $(seq 3); do
+    pks create-cluster "cluster0$i" --num-nodes 1 \
+        --external-hostname "cluster0$i.$external_hostname" \
+        --plan small --non-interactive
+done
